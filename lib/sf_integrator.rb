872528c913e6ae32d693a4aefbfc5f27546ea5cd
Luiz Cezer <lccezinha@gmail.com>
2015-07-08 16:15:24 -0300

    Add generator

--- lib/sf_integrator.rb
+++ lib/sf_integrator.rb
@@ -1,8 +1,9 @@
-require "sf_integrator/version"
-require "sf_integrator/configuration"
-require "sf_integrator/integrator"
-require "sf_integrator/lead"
-require "restforce"
+require 'sf_integrator/engine'
+require 'sf_integrator/version'
+require 'sf_integrator/configuration'
+require 'sf_integrator/integrator'
+require 'sf_integrator/lead'
+require 'restforce'
 
 module SfIntegrator
 end
